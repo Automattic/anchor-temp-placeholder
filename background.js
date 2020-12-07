d05941b034a693b0b8d3a27364519ba46c6cd431
Rosalind W <blackjacksoftwaredev@gmail.com>
2020-12-07 14:55:17 -0600

    Prepend to anchor body

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 function handleClick(tab) {
-    chrome.tabs.executeScript(null, { file: "jquery-3.3.1.min.js" }, function() {
-        chrome.tabs.executeScript(null, { file: "execute.js" });
-    });
+  chrome.tabs.executeScript(null, { file: "jquery-3.3.1.min.js" }, function () {
+    chrome.tabs.executeScript(null, { file: "onExtensionControlClick.js" });
+  });
 }
 
 chrome.browserAction.onClicked.addListener(handleClick);
