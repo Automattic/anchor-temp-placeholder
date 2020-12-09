20cf9d065251c99ccc062a53d78ca54864112dbf
Rosalind W <blackjacksoftwaredev@gmail.com>
2020-12-09 13:31:38 -0600

    All toggle behaviors, page-specific

---
+++ README.md
@@ -0,0 +1,15 @@
+This extension provides temporary placeholder UI on http://anchor.fm to facilitate end-to-end testing of flows leading from anchor.fm into WPCOM.
+
+# To Install Extension
+
+- Check out this repository locally.
+- In Chrome, navigate to chrome://extensions
+- Toggle "Developer Mode" on if it is not already enabled.
+- Click "Load Unpacked" and select the container folder of the repository.
+- If desired, update the `SITE_ROOT`, `PODCAST_ID`, and `EPISODE_ID` values in `onExtensionControlClick.js`, then click the "Refresh" button on the extension page. (Use this "Refresh" button any time you make changes to the content of the extension.)
+
+# To Use Extension
+
+- Navigate to http://anchor.fm and log into an account that has at least one episode created.
+- While on the dashboard, click the extension icon to toggle the "Create a site and transcribe episodes" banner.
+- While on an episode page, click the extension icon to toggle the "Convert to blog post" link
--- onExtensionControlClick.js
+++ onExtensionControlClick.js
@@ -1,16 +1,50 @@
-var container = $("#app-content > div > div > div");
-var bannerHtml = $("<div class='anchor-placeholder-banner'></div>");
-bannerHtml.append(
-  '<img src="https://s.w.org/style/images/about/WordPress-logotype-simplified.png" />'
-);
-bannerHtml.append('<a class="btn" href="#">Get Started</a>');
-bannerHtml.append("<h1>Create a site &amp; transcribe episodes</h1>");
-bannerHtml.append(
-  "<p>Increase your audience reach by creating a site with Wordpress.com.</p>"
-);
+var SITE_ROOT = "http://calypso.localhost:3000";
+var PODCAST_ID = "3e51070";
+var EPISODE_ID = "e5f332f1-a347-40d9-a79b-ce30884057e5";
+var path = window.location.pathname;
 
-if ($(".anchor-placeholder-banner").length) {
-  $(".anchor-placeholder-banner").remove();
-} else {
-  container.prepend(bannerHtml);
+if (path.includes("/dashboard/episode/")) {
+  handleEpisodePage();
+}
+if (path === "/dashboard") {
+  handleDashboard();
+}
+
+function handleDashboard() {
+  var container = $("#app-content > div > div > div");
+  var bannerHtml = $("<div class='anchor-placeholder-banner'></div>");
+  bannerHtml.append(
+    '<img src="https://s.w.org/style/images/about/WordPress-logotype-simplified.png" />'
+  );
+  bannerHtml.append(
+    `<a class="btn" href="${SITE_ROOT}/new?anchor_podcast=${PODCAST_ID}" target="_blank" rel="noopener noreferrer">Get Started</a>`
+  );
+  bannerHtml.append("<h1>Create a site &amp; transcribe episodes</h1>");
+  bannerHtml.append(
+    "<p>Increase your audience reach by creating a site with Wordpress.com.</p>"
+  );
+
+  if ($(".anchor-placeholder-banner").length) {
+    $(".anchor-placeholder-banner").remove();
+  } else {
+    container.prepend(bannerHtml);
+  }
+}
+
+function handleEpisodePage() {
+  var buttonRow = $(
+    "#app-content > div > div > div > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(2)"
+  );
+  var tempControls = $('<div class="anchor-placeholder-controls"></div>');
+  tempControls.append('<a href="#" class="btn">Share episode</a>');
+  tempControls.append(
+    '<a class="blog-post-link">Convert to blog post. <img src="https://s.w.org/style/images/about/WordPress-logotype-simplified.png" /></a>'
+  );
+  if ($(".anchor-placeholder-controls").length) {
+    buttonRow.show();
+    $(".anchor-placeholder-controls").remove();
+  } else {
+    buttonRow.hide();
+    buttonRow.after(tempControls);
+  }
 }
